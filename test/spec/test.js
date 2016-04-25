4ee82a5922fd4b967da1b3dad3ff54a27fec0b11
Gardem <dmitryarchebasov@gmail.com>
2016-04-25 14:23:28 +0300

    Первая инициализация

---
+++ test/spec/test.js
@@ -0,0 +1,12 @@
+(function () {
+  'use strict';
+
+  describe('Give it some context', function () {
+    describe('maybe a bit more context here', function () {
+      it('test should return 1', function () {
+        assert.equal(test(2),1);
+      });
+    });
+  });
+
+}());
